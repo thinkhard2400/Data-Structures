3beaff08890ddb13a03f1ad3f8ee14b77b5adfd5
Lacri <thinkhard2400@gmail.com>
2026-09-12 16:24:14 +0900

    [Solved]

--- Data-Structures/Stack_and_Queue/Q3_C_SQ.c
+++ Data-Structures/Stack_and_Queue/Q3_C_SQ.c
@@ -103,7 +103,23 @@ int main()
 
 int isStackPairwiseConsecutive(Stack *s)
 {
-  /* add your code here */
+    LinkedList ll = s->ll;
+
+    int a;
+    int b;
+
+    if ((ll.size)%2 != 0)
+        return 0;
+
+    while (isEmptyStack(s) == 0)
+    {
+        a = pop(s);
+        b = pop(s);
+
+        if (abs(a - b) != 1)
+            return 0;
+    }
+    return 1;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
