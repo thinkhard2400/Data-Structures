10d672404bc0526d3dbf2d393773cbab442f5f74
Lacri <thinkhard2400@gmail.com>
2026-09-12 22:01:28 +0900

    [Solved]

--- Data-Structures/Binary_Tree/Q1_E_BT.c
+++ Data-Structures/Binary_Tree/Q1_E_BT.c
@@ -114,9 +114,22 @@ int main()
 //////////////////////////////////////////////////////////////////////////////////
 
 int identical(BTNode *tree1, BTNode *tree2)
-
 {
-   /* add your code here */
+    if (tree1 == NULL && tree2 == NULL)
+        return 1;
+
+    if (tree1 == NULL || tree2 == NULL)
+        return 0;
+
+    if (tree1->item != tree2->item)
+        return 0;
+
+    int result1 = identical(tree1->left, tree2->left);
+    int result2 = identical(tree1->right, tree2->right);
+
+    if (result1 == 0 || result2 == 0)
+        return 0;
+    return 1;
 }
 
 /////////////////////////////////////////////////////////////////////////////////
